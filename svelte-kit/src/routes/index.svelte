3d7f2ce718b72d7b7690325ab6f2d3d3a79c521b
Patrick Hellebrand <pat@redesiigner.com>
2022-05-11 09:10:28 +0200

    Initial Commit

---
+++ svelte-kit/src/routes/index.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<template>
+	<header>
+		<p>Boschtmann</p>
+	</header>
+	<main>
+
+	</main>
+	<footer>
+
+	</footer>
+</template>
+
+<style global lang="postcss">
+	#app {
+		@apply flex flex-col justify-center;
+
+		& > header {
+			@apply bg-red-500;
+		}
+	}
+</style>
